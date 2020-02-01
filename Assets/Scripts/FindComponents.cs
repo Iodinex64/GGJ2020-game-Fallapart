d820fab07a2c450f4fe00f3a54f411339757089d
Iodine <sjdsoundworks@gmail.com>
2020-02-01 10:07:08 +0000

    picking up components

---
+++ Assets/Scripts/FindComponents.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindComponents : MonoBehaviour
+{
+    public Transform Ljoint, Rjoint, Ujoint;
+    private bool Lbool, Rbool, Ubool = false;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("pickup"))
+        {
+            collision.GetComponent<BoxCollider2D>().isTrigger = true;
+            collision.transform.position = Rjoint.position;
+            collision.transform.parent = Rjoint.transform;
+            collision.attachedRigidbody.velocity = new Vector2(0f, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/Pellet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pellet : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float speed;
+    public GameObject pl;
+
+    void Start()
+    {
+        pl = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
+        if (pl.GetComponent<PlayerBase>().facingRight)
+        {
+            rb.velocity = transform.right * speed;
+        }
+        else
+        {
+            rb.velocity = (transform.right * -1f) * speed;
+        }
+        Invoke("Kill", 10f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Kill();
+    }
+
+    void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
