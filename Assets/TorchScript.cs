8eb5f115171e92debeeac924fd4ed508bb65f248
Iodine <sjdsoundworks@gmail.com>
2020-02-01 14:35:41 +0000

    implemented torch mechanic

---
+++ Assets/TorchScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class TorchScript : MonoBehaviour
+{
+    public bool isEquipped = false;
+    private Light2D gLight;
+
+    private void Start()
+    {
+        gLight = GameObject.FindGameObjectWithTag("globallight").GetComponent<Light2D>();
+    }
+
+    private void Update()
+    {
+        ManageLightLevels();
+    }
+
+    void ManageLightLevels()
+    {
+        if (isEquipped)
+        {
+            gLight.intensity = 0.45f;
+        }
+        else
+        {
+            gLight.intensity = 0.15f;
+        }
+    }
+
+}
